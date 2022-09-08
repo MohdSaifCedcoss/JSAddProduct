066dcca4c151f889d27262866c1800c33e79a2be
MohdSaifCedcoss <mohdsaif2022aug@cedcoss.com>
2022-09-08 13:37:31 +0530

    Added the styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 function addProduct() {
-  let arr = [];
   let id = document.getElementById("ID").value;
   let name = document.getElementById("name").value;
   let price = document.getElementById("price").value;
   let table = document.getElementById("myTable");
-//   table="<th>Product id\b\b\b\b\</th><th>Product Name\b\b\b\b<\th>Price";
+  if(id=="" || name=="" || price==""){
+    alert("Please fill the details !");
+    return;
+  }
   let row = table.insertRow(0);
   let cell1 = row.insertCell(0);
   let cell2 = row.insertCell(1);
   let cell3 = row.insertCell(2);
-  cell1.innerHTML = id;
-  cell2.innerHTML = name;
-  cell3.innerHTML = price;
-//   document.getElementById("myTable").innerHTML=id;
+  cell1.innerHTML = id+"<br>";
+  cell2.innerHTML = name+"<br>";
+  cell3.innerHTML = price+"<br>";
 }
