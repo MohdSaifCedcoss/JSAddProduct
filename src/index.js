210ea0ee4322179ee07ecaa89144628559618f63
MohdSaifCedcoss <mohdsaif2022aug@cedcoss.com>
2022-09-08 14:56:42 +0530

    Added the comments and done some changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 function addProduct() {
+
+  // Getting and storing the value of product ID in id
   let id = document.getElementById("ID").value;
+
+  // Getting Getting and storing the value of product name in name
   let name = document.getElementById("name").value;
+
+  //Getting and storing the value of product price in price
   let price = document.getElementById("price").value;
+
+  // Creating a table variable on which we will call insertRow function
   let table = document.getElementById("myTable");
+
+  // Checking if the user is giving an empty field
   if(id=="" || name=="" || price==""){
     alert("Please fill the details !");
     return;
   }
+
+  // Creating and inserting a row in the table
   let row = table.insertRow(0);
+
+  // creating three coulumns and inserting in the row
   let cell1 = row.insertCell(0);
   let cell2 = row.insertCell(1);
   let cell3 = row.insertCell(2);
-  cell1.innerHTML = id+"<br>";
-  cell2.innerHTML = name+"<br>";
-  cell3.innerHTML = price+"<br>";
+
+  // assigning the value of id,name and price in all the three coloumns
+  cell1.innerHTML = id;
+  cell2.innerHTML = name;
+  cell3.innerHTML = price;
+
 }
